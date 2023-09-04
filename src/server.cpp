2ff317ce74f386f1b6a5af2d31e28ab998a24f62
MichelH4cker <michel.hecker@usp.br>
2023-09-04 11:42:17 -0300

    feat: make server multithreaded

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,28 @@
 #include "server.hpp"
 
+#define MAX_CONNECTIONS 50
+
+void handle_client(int client_socket){
+    // Communication with the client
+    char buffer[1024];
+    string message;
+    while (true) {
+        // Receive data from the client
+        ssize_t bytesRead = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytesRead <= 0) {
+            break;
+        }
+
+        // receive msg
+        buffer[bytesRead] = '\0';
+        printf("\t - Server side received: %s\n", buffer);
+
+        // send msg
+        message = "Server received message";
+        send(client_socket, message.c_str(), message.length(), 0);
+    }
+}
+
 int main(){
     // CREATE SOCKET DESCRIPTOR 
     int server_socket = socket(AF_INET, SOCK_STREAM, 0); // 0 to tcp protocol
@@ -23,44 +46,33 @@ int main(){
     }
     printf("[+] Socket binded.\n");
 
-    // Listen on the socket, with 40 max connection
-    if (listen(server_socket, 50) == 0){
+    // Listen on the socket, with MAX_CONNECTIONS
+    if (listen(server_socket, MAX_CONNECTIONS) == 0){
         printf("[+] Socket server is listening.\n");
     } else {
         printf("[-] Error in listening\n");
     }
-
-    int new_socket = accept(server_socket, nullptr, nullptr);
-    if (new_socket < 0){
-        perror("[-] Connection failed.\n");
-        exit(EXIT_FAILURE);
-    }
-    printf("[+] Connected.\n");
-        
-
-    // Communication with the client
-    char buffer[1024];
-    string message;
+    
+    vector<thread> clients_threads;
+    
     while (true) {
-        // Receive data from the client
-        ssize_t bytesRead = recv(new_socket, buffer, sizeof(buffer), 0);
-        if (bytesRead <= 0) {
-            break;
+        // Accept incoming connections
+        int new_client_socket = accept(server_socket, nullptr, nullptr);
+        if (new_client_socket == -1) {
+            cerr << "[-] Error accepting connection" << endl;
+            continue;
         }
 
-        // receive msg
-        buffer[bytesRead] = '\0';
-        printf("\t - Server side received: %s\n", buffer);
+        cout << "[+] Client connected: " << new_client_socket << endl;
 
-        // send msg
-        message = "Server received message";
-        send(new_socket, message.c_str(), message.length(), 0);
+        // add new client to threads vector
+        // handle new client
+        clients_threads.emplace_back(handle_client, new_client_socket);
     }
 
 
     // CLOSE SOCKET
     close(server_socket);
-    close(new_socket);
-
+    
     return 0;
 }
--- src/server.hpp
+++ src/server.hpp
@@ -13,6 +13,7 @@ using namespace std;
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
+#include <thread>
+#include <vector>
 
 #endif
