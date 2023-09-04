1755eb2e4a81452d00b34ee7ba164fd3926a3380
MichelH4cker <michel.hecker@usp.br>
2023-09-04 09:33:50 -0300

    chore: create files and gitignore

---
+++ src/client.hpp
@@ -0,0 +1,13 @@
+#ifndef __CLIENT_HPP__
+#define __CLIENT_HPP__
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace std;
+
+#endif
---
+++ src/server.hpp
@@ -0,0 +1,13 @@
+#ifndef __SERVER_HPP__
+#define __SERVER_HPP__
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace std;
+
+#endif
